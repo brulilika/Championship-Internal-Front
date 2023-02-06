bb3eb1786f7b8b725f6eb80c39535f298874ace5
Proxyturn <william.a.finardi@gmail.com>
2023-02-06 16:50:21 -0300

    alteração

---
+++ Pages/Championships/Championship.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Championship_Internal_Front.Pages
+{
+    public class ChampionshipModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
